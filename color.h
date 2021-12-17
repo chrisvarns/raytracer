e3751b656f5f68b545dd79116302a13be58ba00b
Chris Varnsverry <chrisvarnz@gmail.com>
2021-12-17 01:56:37 +0000

    8.3 gamma correction (still quite dark for some reason)

--- color.h
+++ color.h
@@ -3,9 +3,9 @@
 void write_color(unsigned char*& out, const color& pixel_color, int samples_per_pixel)
 {
 	const auto scale = 1.0/samples_per_pixel;
-	const auto r = pixel_color.r * scale;
-	const auto g = pixel_color.g * scale;
-	const auto b = pixel_color.b * scale;
+	const auto r = sqrt(scale * pixel_color.r);
+	const auto g = sqrt(scale * pixel_color.g);
+	const auto b = sqrt(scale * pixel_color.b);
 
 	*out++ = static_cast<unsigned char>(256 * clamp(r, 0.0, 0.999));
 	*out++ = static_cast<unsigned char>(256 * clamp(g, 0.0, 0.999));
