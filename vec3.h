47f825d63322ac70f5dc16b05b691353410841ac
Chris Varnsverry <chrisvarnz@gmail.com>
2021-12-17 00:47:35 +0000

    3.2 vec3 utility functions, but modified a bit to my taste

--- vec3.h
+++ vec3.h
@@ -8,30 +8,26 @@ using std::sqrt;
 class vec3
 {
 public:
-	vec3() : e{0,0,0} {}
-	vec3(double e0, double e1, double e2) : e{e0, e1, e2} {}
+	vec3() = default;
+	vec3(double x_, double y_, double z_) : x(x_), y(y_), z(z_) {}
 
-	double x() const { return e[0]; }
-	double y() const { return e[1]; }
-	double z() const { return e[2]; }
-
-	vec3 operator-() const { return vec3(-e[0], -e[1], -e[2]); }
-	double operator[](const int i) const { return e[1]; }
-	double& operator[](const int i) { return e[1]; }
+	vec3 operator-() const { return vec3(-x, -y, -z); }
+	double operator[](const int i) const { return y; }
+	double& operator[](const int i) { return y; }
 
 	vec3& operator+=(const vec3& v)
 	{
-		e[0] += v.e[0];
-		e[1] += v.e[1];
-		e[2] += v.e[2];
+		x += v.x;
+		y += v.y;
+		z += v.z;
 		return *this;
 	}
 
 	vec3& operator*=(const double t)
 	{
-		e[0] *= t;
-		e[1] *= t;
-		e[2] *= t;
+		x *= t;
+		y *= t;
+		z *= t;
 		return *this;
 	}
 
@@ -47,10 +43,59 @@ public:
 
 	double length_squared() const
 	{
-		return e[0]*e[0] + e[1]*e[1] + e[2]*e[2];
+		return x*x + y*y + z*z;
 	}
 };
 
 // Type aliases for vec3
 using point3 = vec3;	// 3D point
 using color = vec3;		// RGB color
+
+inline vec3 operator+(const vec3& u, const vec3& v)
+{
+	return vec3(u.x + v.x, u.y + v.y, u.z + v.z);
+}
+
+inline vec3 operator-(const vec3& u, const vec3& v)
+{
+	return vec3(u.x - v.x, u.y - v.y, u.z - v.z);
+}
+
+inline vec3 operator*(const vec3& u, const vec3& v)
+{
+	return vec3(u.x * v.x, u.y * v.y, u.z * v.z);
+}
+
+inline vec3 operator*(double t, const vec3& v)
+{
+	return vec3(t * v.x, t * v.y, t * v.z);
+}
+
+inline vec3 operator*(const vec3& v, double t)
+{
+	return t * v;
+}
+
+inline vec3 operator/(const vec3& v, double t)
+{
+	return (1/t) * v;
+}
+
+inline double dot(const vec3& u, const vec3& v)
+{
+	return u.x * v.x
+		 + u.y * v.y
+		 + u.z * v.z;
+}
+
+inline vec3 cross(const vec3& u, const vec3& v)
+{
+	return vec3(u.y * v.z - u.z * v.y,
+				u.z * v.x - u.x * v.z,
+				u.x * v.y - u.y * v.x);
+}
+
+inline vec3 normalize(const vec3& v)
+{
+	return v/v.length();
+}
