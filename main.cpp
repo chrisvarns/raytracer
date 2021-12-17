893255b1f189081ee13a386a6eca0cacc874be10
Chris Varnsverry <chrisvarnz@gmail.com>
2021-12-17 00:46:55 +0000

    save to image.png with stb_image_write

--- main.cpp
+++ main.cpp
@@ -1,12 +1,14 @@
-#include <iostream>
+#include "stb_image_write.h"
 
 int main()
 {
 	const int image_width = 256;
 	const int image_height = 256;
+	const char* image_filename = "image.png";
 
-	std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
-
+	unsigned char image_data[image_width*image_height*3];
+	
+	unsigned char* image_data_ptr = image_data;
 	for (int j = image_height - 1; j >= 0; --j)
 	{
 		for (int i = 0; i < image_width; ++i)
@@ -15,11 +17,11 @@ int main()
 			auto g = double(j) / (image_height-1);
 			auto b = 0.25;
 
-			int ir = static_cast<int>(255.999 * r);
-			int ig = static_cast<int>(255.999 * g);
-			int ib = static_cast<int>(255.999 * b);
-
-			std::cout << ir << " " << ig << " " << ib << "\n";
+			*image_data_ptr++ = static_cast<unsigned char>(255.999 * r);
+			*image_data_ptr++ = static_cast<unsigned char>(255.999 * g);
+			*image_data_ptr++ = static_cast<unsigned char>(255.999 * b);
 		}
 	}
+
+	stbi_write_png(image_filename, image_width, image_height, 3, image_data, 0);
 }
