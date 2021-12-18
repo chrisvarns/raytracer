7c77222e6a17eadb757eecab67a4fa5e0fd6fada
Chris Varnsverry <chrisvarnz@gmail.com>
2021-12-18 19:25:09 +0000

    4.3 checker on the ground

--- main.cpp
+++ main.cpp
@@ -55,8 +55,8 @@ hittable_list random_scene()
 {
 	hittable_list world;
 
-	const auto ground_material = make_shared<lambertian>(color(0.5, 0.5, 0.5));
-	world.add(make_shared<sphere>(point3(0, -1000,0), 1000, ground_material));
+	const auto checker = make_shared<checker_texture>(color(0.2, 0.3, 0.1), color(0.9, 0.9, 0.9));
+	world.add(make_shared<sphere>(point3(0, -1000,0), 1000, make_shared<lambertian>(checker)));
 
 	const auto mat_dielectric = make_shared<dielectric>(1.5);
 	world.add(make_shared<sphere>(point3(0, 1, 0), 1.0, mat_dielectric));
@@ -65,8 +65,8 @@ hittable_list random_scene()
 	const auto mat_metal = make_shared<metal>(color(0.7, 0.6, 0.5), 0.0);
 	world.add(make_shared<sphere>(point3(4, 1, 0), 1.0, mat_metal));
 
-	for(int a = -5; a < 5; a++) {
-		for(int b = -5; b < 5; b++) {
+	for(int a = -11; a < 11; a++) {
+		for(int b = -11; b < 11; b++) {
 			const auto choose_mat = random_float();
 			point3 center(a + 0.9*random_float(), 0.2, b + 0.9*random_float());
 			if ((center - point3(4, 0.2, 0)).length() > 0.9) {
--- texture.h
+++ texture.h
@@ -19,3 +19,20 @@ public:
 
 	color color_value;
 };
+
+class checker_texture : public texture
+{
+public:
+	checker_texture() {}
+	checker_texture(const shared_ptr<texture>& _even, const shared_ptr<texture>& _odd) : even(_even), odd(_odd) {}
+	checker_texture(const color& c1, const color& c2) : even(make_shared<solid_color>(c1)), odd(make_shared<solid_color>(c2)) {}
+
+	virtual color value(float u, float v, const point3& p) const override
+	{
+		const auto sines = sin(10*p.x) * sin(10*p.y) * sin(10*p.z);
+		const auto& selected = sines < 0 ? odd : even;
+		return selected->value(u, v, p);
+	}
+
+	shared_ptr<texture> odd, even;
+};
